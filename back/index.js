43eece2593c7230afedef6e9b38be9151372df0f
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-11-09 18:10:02 +0300

    Separate folder for backend

--- back/index.js
+++ back/index.js
@@ -8,6 +8,8 @@ mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.soz1hvz.mongodb.net/health
 .then(() => console.log("DB OK"))
 .catch((err) => console.log("DB error", err));
 
+const PORT = process.env.PORT || 4444;
+
 const app = express();
 
 app.use(express.json());
@@ -16,8 +18,9 @@ app.post('/auth/login', login)
 
 app.post('/auth/register', registerValidation, register);
 
+app.get('/auth/me', checkAuth, getMe);
 
-app.listen(4444, (err) => {
+app.listen(PORT, (err) => {
   if (err){
     console.log(err);
   }
