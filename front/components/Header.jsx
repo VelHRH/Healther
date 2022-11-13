223cc21dee7afa04e27cafdcb6470478db9bce3b
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-11-13 18:56:42 +0300

    Header created

---
+++ front/components/Header.jsx
@@ -0,0 +1,23 @@
+import { FireIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+
+export const Header = () => {
+ return (
+  <div className="w-full flex border-b-4 border-violet-400 h-20 items-center px-20 justify-between">
+   <div className="font-bold text-transparent text-5xl bg-clip-text bg-gradient-to-r to-violet-500 from-violet-800 logo">
+    healther
+   </div>
+   <div className="flex">
+    <div className="text-orange-500 hover:text-orange-300 flex text-2xl cursor-pointer items-center mr-7">
+     <FireIcon className="w-8" />
+     <div>112</div>
+    </div>
+
+    <img
+     src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+     alt="Profile"
+     className="w-14 rounded-[50%] cursor-pointer"
+    />
+   </div>
+  </div>
+ );
+};
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Header } from "../components/Header"
 
 export default function Home() {
   return (
-    <div className='bg-slate-500'>
-      Привет
-    </div>
+    <>
+    <Header />
+    </>
   )
 }
