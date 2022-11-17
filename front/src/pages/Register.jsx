349115ef294755cca4663fd6035f407543a83a6f
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-11-17 11:41:00 +0300

    Register form

---
+++ front/src/pages/Register.jsx
@@ -0,0 +1,32 @@
+export function Register() {
+ return (
+  <div className="bg-gradient-to-r from-violet-500 to-purple-500 w-screen h-screen">
+   <div className="flex flex-col px-10 w-11/12 md:w-1/3 left-[50%] top-[50%] absolute bg-slate-100 translate-y-[-50%] items-center translate-x-[-50%] text-xl rounded-xl">
+    <div className="my-5 text-2xl">Register</div>
+    <input
+     type="text"
+     placeholder="Your email..."
+     className="bg-slate-100 w-full border-b-2 border-violet-400 my-5 outline-none focus:border-violet-800"
+    ></input>
+    <input
+     type="text"
+     placeholder="Full name..."
+     className="bg-slate-100 w-full border-b-2 border-violet-400 my-5 outline-none focus:border-violet-800"
+    ></input>
+    <input
+     type="password"
+     placeholder="Password..."
+     className="bg-slate-100 w-full border-b-2 border-violet-400 my-5 outline-none focus:border-violet-800"
+    ></input>
+    <input
+     type="text"
+     placeholder="Avatar URL..."
+     className="bg-slate-100 w-full border-b-2 border-violet-400 my-5 outline-none focus:border-violet-800"
+    ></input>
+    <div className="bg-violet-700 text-slate-50 p-2 px-5 my-5 w-full ease-in duration-200 text-center rounded-full cursor-pointer hover:bg-transparent hover:text-violet-700 border-2 border-violet-700">
+     Register account
+    </div>
+   </div>
+  </div>
+ );
+}
